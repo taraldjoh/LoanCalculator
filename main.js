d96727aca0e923472e054c99a7d65d94b8765742
taraldjoh <tarald.johansen@outlook.com>
2019-03-10 19:24:34 +0100

    added loading function, shows and hides

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 // Listen for submit
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  // Hide Results
+  document.getElementById("results").style.display = "none";
+  // Show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating...");
 
   // UI Vars
@@ -27,15 +34,24 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show Results
+    document.getElementById("results").style.display = "block";
+
+    // Hide Loader
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers");
   }
-
-  e.preventDefault();
 }
 
 // ShowError
 function showError(error) {
+  // Show Results
+  document.getElementById("results").style.display = "hide";
+
+  // Hide Loader
+  document.getElementById("loading").style.display = "none";
   //Create div
   const errorDiv = document.createElement("div");
 
